57b4211d534510be9e6df492c0efd627052283d9
JimiDorji <127373407+JimiDorji@users.noreply.github.com>
2024-12-09 12:55:59 +0600

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)  # Note the correct use of '__name__' (not '_name__')
-
-@app.route('/')
-def hello_world():
-    return 'Hello Jigme'
-
-if __name__ == '__main__':
-    app.run()  # Correct method to start the Flask server
